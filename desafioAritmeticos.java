0f6dc841eda2269ea030739780b51a3cfa6def2a
marciofreir <144838846+marciofreir@users.noreply.github.com>
2023-10-17 16:26:49 -0300

    Add files via upload

---
+++ desafioAritmeticos.java
@@ -0,0 +1,24 @@
+package oii;
+
+public class desafioAritmeticos {
+    public static void main(String[] args) {
+        int a = (3+2);
+        int b = 6;
+        int c = (b * a /2);
+        int d = (int)Math.pow (c, 2);
+
+        int e = d / 3*2;
+
+        int f = (1-5) * (2-7)/2;
+        int g = (int)Math.pow (f, 2);
+
+        int h = e - g;
+        int i = (int)Math.pow(h, 3);
+
+        int j = 10;
+        int k = (int)Math.pow(j, 3);
+
+        int l = i / k;
+        System.out.println(l);
+    }
+}
